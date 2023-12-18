04d529ba7fb8caf62edbdca153c244e907797cd3
turnerturn <43834067+turnerturn@users.noreply.github.com>
2023-12-17 23:05:32 -0600

    Update TouchscreenVariable.java

--- touchscreen-api/src/main/java/hello/TouchscreenVariable.java
+++ touchscreen-api/src/main/java/hello/TouchscreenVariable.java
@@ -1,17 +1,19 @@
 package sandbox;
-import java.util.Map;
+
 public class TouchscreenVariable {
 
 	private String name;
 	private String value;
-	private String maxBits;
-
-	//constructors
-	public TouchscreenVariable() {
-	}
-	public TouchscreenVariable(String name, String value) {
+	private String dataType;
+	private String ioAddress;
+	private Integer maxCount;
+public TouchscreenVariable() {}
+	public TouchscreenVariable(String name, String value, String dataType, String ioAddress, Integer maxCount) {
 		this.name = name;
 		this.value = value;
+		this.dataType = dataType;
+		this.ioAddress = ioAddress;
+		this.maxCount = maxCount;
 	}
 	//getter
 	public String getName() {
@@ -29,17 +31,33 @@ public class TouchscreenVariable {
 	public void setValue(String value) {
 		this.value = value;
 	}
-//getter
-	public String getMaxBits() {
-		return maxBits;
+	//getter
+	public String getDataType() {
+		return dataType;
 	}
 	//setter
-	public void setMaxBits(String maxBits) {
-		this.maxBits = maxBits;
+	public void setDataType(String dataType) {
+		this.dataType = dataType;
 	}
-	@Override
+	//getter
+	public String getIoAddress() {
+		return ioAddress;
+	}
+	//setter
+	public void setIoAddress(String ioAddress) {
+		this.ioAddress = ioAddress;
+	}
+	//getter
+	public Integer getMaxCount() {
+		return maxCount;
+	}
+	//setter
+	public void setMaxCount(Integer maxCount) {
+		this.maxCount = maxCount;
+	}
+	//tostring
 	public String toString() {
-		return String.format("TouchscreenVariable{name=%s, value=%s}", getName(), getValue());
+		return "TouchscreenVariable(name=" + getName() + ", value=" + getValue() + ", dataType=" + getDataType()
+				+ ", ioAddress=" + getIoAddress() + ", maxCount=" + getMaxCount() + ")";
 	}
-	 
 }
