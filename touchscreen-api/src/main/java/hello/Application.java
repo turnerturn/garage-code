6883e53891ddb4c324a15a4e88308d5d3e431141
turnerturn <43834067+turnerturn@users.noreply.github.com>
2023-12-17 23:04:33 -0600

    Update Application.java

--- touchscreen-api/src/main/java/hello/Application.java
+++ touchscreen-api/src/main/java/hello/Application.java
@@ -1,8 +1,6 @@
 
 package sandbox;
 
-import jakarta.jms.ConnectionFactory;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
@@ -16,13 +14,7 @@ import org.springframework.jms.support.converter.MappingJackson2MessageConverter
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
 
-
-import java.util.Map;
-import java.util.Arrays;
-import java.util.HashMap;
-
-import org.springframework.jms.core.MessageCreator;
-import org.springframework.jms.core.JmsTemplate;
+import jakarta.jms.ConnectionFactory;
 @SpringBootApplication
 @EnableJms
 public class Application {
@@ -45,39 +37,17 @@ public class Application {
 		converter.setTypeIdPropertyName("_type");
 		return converter;
 	}
-
-/*
- *     
- *  Map<String,String> map = new HashMap();
- *  map.put("Name", "Mark");
-    map.put("Age", new Integer(47));
-    jmsTemplate.convertAndSend("testQueue", map, new MessagePostProcessor() {
-        public Message postProcessMessage(Message message) throws JMSException {
-            message.setIntProperty("AccountID", 1234);
-            message.setJMSCorrelationID("123-00001");
-            return message;
-        }
-    });
- */
 	public static void main(String[] args) {
 		// Launch the application
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 
 		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-		Map<String,Object> tags = new HashMap();
-		//TODO this can be stored by sender to track lifecycle of specific meesage.
-		String correlationId = "";//UUID.randomUUID().toString();
-		tags.put("CORRELATION_ID", correlationId);
-		tags.put("VARIABLE_NAME", "VAR_1");
-		tags.put("POLLING_TIMEOUT_MILLIS", 60000);
-		tags.put("POLLING_VALUES", Arrays.asList("done","abort","page","manual_entry"));
-		tags.put("POLLING_ReplyTo", "display-assistant-polling-message");
 
 		// Send a message with a POJO - the template reuse the message converter
 		System.out.println("Sending a touchscreen-memory-polling message to upload touchscreen variable.");
-		jmsTemplate.convertAndSend("touchscreen-memory-polling", new TouchscreenMemoryMessage("VAR_1","clickclickboom",tags), m -> {
+		jmsTemplate.convertAndSend("touchscreen-variables-download","clickclickboom", m -> {
 
-            m.setJMSCorrelationID(correlationId);
+            m.setJMSCorrelationID("correlationId");
             m.setJMSExpiration(60000);//TODO configure this
             //m.setJMSReplyTo(new ActiveMQQueue(ORDER_QUEUE));
             //m.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT);
@@ -85,10 +55,6 @@ public class Application {
             m.setJMSTimestamp(System.nanoTime());
             m.setJMSType("type");
 			m.setStringProperty("VARIABLE_NAME", "VAR_1");
-			m.setIntProperty("POLLING_TIMEOUT_MILLIS", 60000);
-			m.setStringProperty("POLLING_VALUES", "done,abort,page,manual_entry");
-			m.setStringProperty("POLLING_ReplyTo", "display-assistant-polling-message");
-	
            
             return m;
         });
